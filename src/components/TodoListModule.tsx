07996554c63ad64c7380f3e0fabdc0eed74b05b9
dherv <damien.herv@gmail.com>
2020-05-30 14:48:33 +0900

    style(todo): update TodoListModule wrapper

--- src/components/TodoListModule.tsx
+++ src/components/TodoListModule.tsx
@@ -2,10 +2,11 @@ import React, { FC } from "react";
 import { ITodo } from "../types/interfaces";
 import TodoListTitle from "./TodoListTitle";
 import TodoList from "./TodoList";
+import styles from "./TodoListModule.module.css";
 
 const TodoListModule: FC<Props> = ({ title, todos, onToggleEdit }) => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <TodoListTitle title={title} />
       <TodoList todos={todos} onToggleEdit={onToggleEdit} />
     </div>
